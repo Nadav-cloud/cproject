265775e3318648d5350a1c26ec295a0f9c9e4159
GantonL <45093712+GantonL@users.noreply.github.com>
2021-12-20 21:12:47 +0200

    initials tasks

---
+++ test.py
@@ -0,0 +1,7 @@
+import requests
+endpoint = sdfkjhg
+params = skdjrgh
+daysbehind = user input
+
+response = requests.get('https://corona.lmao.ninja/v2/{endpoint}:kiuswhvf', params=daysbehind)
+print(response.json())
