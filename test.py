f633f941767e76e6ded22dddc75d21025816e867
GantonL <45093712+GantonL@users.noreply.github.com>
2021-12-27 21:32:13 +0200

    api examples

--- test.py
+++ test.py
@@ -1,7 +1,21 @@
 import requests
-endpoint = sdfkjhg
-params = skdjrgh
-daysbehind = user input
+# continent = 'Asia'
+# response = requests.get(f'https://corona.lmao.ninja/v2/continents/{continent}?yesterday&strict')
+# print(response.json())
+# countries = 'Israel,Italy'
+# response = requests.get(f'https://corona.lmao.ninja/v2/countries/{countries}?yesterday')
+# res = response.json()
+# for c in res:
+#     if (c['country'] == 'Israel'):
+#         print (c['cases'])
 
-response = requests.get('https://corona.lmao.ninja/v2/{endpoint}:kiuswhvf', params=daysbehind)
+lastDays = 2
+# response = requests.get(f'https://corona.lmao.ninja/v2/historical?lastdays={lastDays}')
+# print(response.json())
+# state='new york'
+# response = requests.get(f'https://corona.lmao.ninja/v2/historical/usacounties/{state}?lastdays={lastDays}')
+# print(response.json())
+response = requests.get(f'https://covid19.richdataservices.com/rds/api/query/int/jhu_country/select?cols=date_stamp,cnt_death,cnt_recovered&where=(iso3166_1=US)&format=amcharts&limit=5000')
 print(response.json())
+
+
